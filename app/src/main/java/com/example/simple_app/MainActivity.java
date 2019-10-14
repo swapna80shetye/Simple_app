269fdf23f73b38ceb85221607c56a9889cd85d20
Swapna <sw@frisq.se>
2019-10-14 15:51:38 +0200

    added plus minus buttons, changed mainactivity.java

--- app/src/main/java/com/example/simple_app/MainActivity.java
+++ app/src/main/java/com/example/simple_app/MainActivity.java
@@ -4,13 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
-    TextView dt;
+    TextView dt, ft;
+    EditText edT;
+    int length = 150;
+    double val = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +28,42 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void displayText(View view) {
-        display("Swapan");
+        // edT = (EditText) findViewById(R.id.edit_text);
 
-    }
-    private void display(String name){
-        dt = (TextView) findViewById(R.id.display_text);
-        dt.setText(name);
+        display(length);
     }
 
+    private void display(int length) {
+        dt = (TextView) findViewById(R.id.edit_text);
+        dt.setText("" + length);
+
+    }
+
+    private void displayFeet(double val) {
+        ft = (TextView) findViewById(R.id.display_text);
+        ft.setText(String.format(Locale.US, "%.1f", val));
+    }
+
+
+    public void increment(View view) {
+        length += 1;
+        display(length);
+    }
+
+    public void decrement(View view) {
+        length -= 1;
+        display(length);
+    }
+
+    public void length_in_feet(View view) {
+        double val = (double)length/30;
+        displayFeet(val);
+    }
+
+    public void reset_all(View view) {
+        display(150);
+        displayFeet(5.0);
+        length=150;
+        val=0.0;
+    }
 }
